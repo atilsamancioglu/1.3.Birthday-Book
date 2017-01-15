3bbddcc2f9056b13c3399254f12bd607b9dfb345
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-15 15:24:45 +0300

    final commit

--- Birthday Book/ViewController.swift
+++ Birthday Book/ViewController.swift
@@ -7,19 +7,119 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var nameArray = [String]()
+    var ageArray = [Int]()
+    var birthdayArray = [String]()
+    var imageArray = [UIImage]()
+    var selectedFriend = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+  
+        retrieveInfo()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.retrieveInfo), name: NSNotification.Name(rawValue:"friendcreated"), object: nil)
+    }
+    
+    func retrieveInfo() {
+        
+        self.nameArray.removeAll(keepingCapacity: false)
+        self.ageArray.removeAll(keepingCapacity: false)
+        self.imageArray.removeAll(keepingCapacity: false)
+        self.birthdayArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Friends")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let birthday = result.value(forKey: "birthday") as? String {
+                        self.birthdayArray.append(birthday)
+                    }
+                    
+                    if let age = result.value(forKey: "age") as? Int {
+                        self.ageArray.append(age)
+                    }
+                    
+                    if let imageData = result.value(forKey: "image") as? Data {
+                        let image = UIImage(data: imageData)
+                        self.imageArray.append(image!)
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                
+            }
+            
+            
+        } catch {
+            print("an error occured")
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.selectedFriend = nameArray[indexPath.row]
+        performSegue(withIdentifier: "createFriendSegue", sender: nil)
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createFriendSegue" {
+            let destinationVC = segue.destination as! createFriendVC
+            destinationVC.chosenFriend = selectedFriend
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addButtonClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "createFriendSegue", sender: nil)
+        
     }
-
+    
+    
+    
 
 }
 
--- Birthday Book/createFriendVC.swift
+++ Birthday Book/createFriendVC.swift
@@ -7,29 +7,121 @@
 //
 
 import UIKit
+import CoreData
 
-class createFriendVC: UIViewController {
-
+class createFriendVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var ageText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    
+    var chosenFriend = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.layoutIfNeeded()
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
 
-        // Do any additional setup after loading the view.
-    }
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(createFriendVC.selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        if chosenFriend != "" {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Friends")
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenFriend)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                                nameText.text = name
+                        }
+                        
+                        if let birthday = result.value(forKey: "birthday") as? String {
+                                birthdayText.text = birthday
+                        }
+                        
+                        if let age = result.value(forKey: "age") as? Int {
+                                ageText.text = String(age)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                                self.imageView.image = image
+                        }
+                        
+                    }
+                    
+                }
+                
+                
+            } catch {
+                print("an error occured")
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            
+        }
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func selectImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newFriend = NSEntityDescription.insertNewObject(forEntityName: "Friends", into: context)
+        
+        newFriend.setValue(nameText.text, forKey: "name")
+        newFriend.setValue(Int(ageText.text!)!, forKey: "age")
+        newFriend.setValue(birthdayText.text, forKey: "birthday")
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        newFriend.setValue(data, forKey: "image")
+        
+        do {
+            
+            try context.save()
+            print("we managed to save the friend")
+            
+        } catch {
+            
+            print("an error occured!")
+            
+        }
+     
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue:"friendcreated"), object: nil)
+        _ = self.navigationController?.popViewController(animated: true)
+        
+        
+    }
+ 
 }
